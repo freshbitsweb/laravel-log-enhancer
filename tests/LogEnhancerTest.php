c8a0acd059a740a0cebb6252850447de74325882
Gaurav Makhecha <gauravmakhecha@gmail.com>
2018-12-17 16:47:26 +0530

    Add more tests

--- tests/LogEnhancerTest.php
+++ tests/LogEnhancerTest.php
@@ -32,4 +32,41 @@ class LogEnhancerTest extends TestCase
             }
         }
     }
+
+    /** @test */
+    public function it_skips_input_details_as_per_the_configuration()
+    {
+        $record = [];
+
+        config(['laravel_log_enhancer.log_input_data' => false]);
+
+        $requestDataProcessor = new RequestDataProcessor;
+        $record = $requestDataProcessor($record);
+
+        $this->assertArrayNotHasKey('headers', $record['extra']);
+    }
+
+    /** @test */
+    public function it_adds_other_details_as_per_the_configuration()
+    {
+        $record = [];
+
+        config(['laravel_log_enhancer.log_request_headers' => rand(0, 1)]);
+        config(['laravel_log_enhancer.log_session_data' => rand(0, 1)]);
+
+        $requestDataProcessor = new RequestDataProcessor;
+        $record = $requestDataProcessor($record);
+
+        if (config('laravel_log_enhancer.log_request_headers')) {
+            $this->assertArrayHasKey('headers', $record['extra']);
+        } else {
+            $this->assertArrayNotHasKey('headers', $record['extra']);
+        }
+
+        if (config('laravel_log_enhancer.log_session_data')) {
+            $this->assertArrayHasKey('session', $record['extra']);
+        } else {
+            $this->assertArrayNotHasKey('session', $record['extra']);
+        }
+    }
 }
