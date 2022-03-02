7639533485e016ec642a3f34aa0522b9fc79d4a7
Hiren <hiren@freshbits.in>
2022-03-02 19:04:58 +0530

    Update tests

--- tests/LogEnhancerTest.php
+++ tests/LogEnhancerTest.php
@@ -20,45 +20,21 @@ class LogEnhancerTest extends TestCase
         $handlers = $logger->getHandlers();
         foreach ($handlers as $handler) {
             $this->assertInstanceOf(RequestDataProcessor::class, $handler->popProcessor());
-
-            if (config('laravel_log_enhancer.log_memory_usage')) {
-                $this->assertInstanceOf(MemoryUsageProcessor::class, $handler->popProcessor());
-            }
-
-            if (config('laravel_log_enhancer.log_request_details')) {
-                $this->assertInstanceOf(WebProcessor::class, $handler->popProcessor());
-            }
+            $this->assertInstanceOf(MemoryUsageProcessor::class, $handler->popProcessor());
+            $this->assertInstanceOf(WebProcessor::class, $handler->popProcessor());
         }
     }
 
     /** @test */
-    public function it_will_not_adds_request_details_to_logs()
+    public function it_will_not_add_any_processor_to_the_logs()
     {
-        config(['laravel_log_enhancer.log_memory_usage' => true]);
+        config(['laravel_log_enhancer.log_memory_usage' => false]);
         config(['laravel_log_enhancer.log_request_details' => false]);
         $logger = $this->app[Logger::class];
 
         $handlers = $logger->getHandlers();
         foreach ($handlers as $handler) {
             $this->assertInstanceOf(RequestDataProcessor::class, $handler->popProcessor());
-            $this->assertInstanceOf(MemoryUsageProcessor::class, $handler->popProcessor());
-
-            $this->expectException(LogicException::class);
-            $handler->popProcessor();
-        }
-    }
-
-    /** @test */
-    public function it_will_not_adds_memory_usage_details_to_logs()
-    {
-        config(['laravel_log_enhancer.log_memory_usage' => false]);
-        config(['laravel_log_enhancer.log_request_details' => true]);
-        $logger = $this->app[Logger::class];
-
-        $handlers = $logger->getHandlers();
-        foreach ($handlers as $handler) {
-            $this->assertInstanceOf(RequestDataProcessor::class, $handler->popProcessor());
-            $this->assertInstanceOf(WebProcessor::class, $handler->popProcessor());
 
             $this->expectException(LogicException::class);
             $handler->popProcessor();
@@ -83,34 +59,42 @@ class LogEnhancerTest extends TestCase
     {
         $record = [];
 
-        config(['laravel_log_enhancer.log_request_headers' => rand(0, 1)]);
-        config(['laravel_log_enhancer.log_session_data' => rand(0, 1)]);
+        config(['laravel_log_enhancer.log_request_headers' => true]);
+        config(['laravel_log_enhancer.log_session_data' => true]);
+        config(['laravel_log_enhancer.log_git_data' => true]);
+        config(['laravel_log_enhancer.log_app_details' => true]);
 
         $requestDataProcessor = new RequestDataProcessor;
         $record = $requestDataProcessor($record);
 
-        if (config('laravel_log_enhancer.log_request_headers')) {
-            $this->assertArrayHasKey('headers', $record['extra']);
-        } else {
-            $this->assertArrayNotHasKey('headers', $record['extra']);
-        }
+        $this->assertArrayHasKey('headers', $record['extra']);
 
-        if (config('laravel_log_enhancer.log_session_data')) {
-            $this->assertArrayHasKey('session', $record['extra']);
-        } else {
-            $this->assertArrayNotHasKey('session', $record['extra']);
-        }
+        $this->assertArrayHasKey('session', $record['extra']);
 
-        if (config('laravel_log_enhancer.log_git_data')) {
-            $this->assertArrayHasKey('git', $record['extra']);
-        } else {
-            $this->assertArrayNotHasKey('git', $record['extra']);
-        }
+        $this->assertArrayHasKey('git', $record['extra']);
 
-        if (config('laravel_log_enhancer.log_app_details')) {
-            $this->assertArrayHasKey('Application Details', $record['extra']);
-        } else {
-            $this->assertArrayNotHasKey('Application Details', $record['extra']);
-        }
+        $this->assertArrayHasKey('Application Details', $record['extra']);
+    }
+
+    /** @test */
+    public function it_will_not_add_other_details_as_per_the_configuration()
+    {
+        $record = [];
+
+        config(['laravel_log_enhancer.log_request_headers' => false]);
+        config(['laravel_log_enhancer.log_session_data' => false]);
+        config(['laravel_log_enhancer.log_git_data' => false]);
+        config(['laravel_log_enhancer.log_app_details' => false]);
+
+        $requestDataProcessor = new RequestDataProcessor;
+        $record = $requestDataProcessor($record);
+
+        $this->assertArrayNotHasKey('headers', $record['extra']);
+
+        $this->assertArrayNotHasKey('session', $record['extra']);
+
+        $this->assertArrayNotHasKey('git', $record['extra']);
+
+        $this->assertArrayNotHasKey('Application Details', $record['extra']);
     }
 }
